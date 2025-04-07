39a698f5fa21460407ffc34459bff2220594f8e9
Sp1cyP3pp3r <niktoktoto21@gmail.com>
2025-04-07 22:00:19 +0400

    07 04

--- objects/sound/sound_manager.gd
+++ objects/sound/sound_manager.gd
@@ -10,8 +10,12 @@ extends Node
 
 func monstro_appeared() -> void:
 	noticed_monster.play()
+	
+	shake_player_screen(0.3)
+
+func shake_player_screen(ammount : float = 0) -> void:
 	var _red = player.shake_camera.trauma_reduction_rate
 	player.shake_camera.trauma_reduction_rate = 0
-	player.shake_camera.add_trauma(0.5)
+	player.shake_camera.add_trauma(ammount)
 	await get_tree().create_timer(0.3).timeout
 	player.shake_camera.trauma_reduction_rate = _red
